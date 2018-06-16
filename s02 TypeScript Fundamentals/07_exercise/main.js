efc6467cdd1dfe85e9dea005d84d6fb6ab3eb9e8
valeriorisuleo <valerio.risuleo@gmail.com>
2018-06-16 02:02:30 +0200

    angular

--- s02 TypeScript Fundamentals/07_exercise/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var like_1 = require("./like");
-var clickMe = new like_1.Like(true, 200);
-clickMe.onClick();
-console.log(clickMe.displayResults());
--- s02 TypeScript Fundamentals/07_exercise/main.ts
+++
@@ -1,6 +0,0 @@
-import { Like } from './like';
-
-let clickMe = new Like(true, 200);
-clickMe.onClick();
-
-console.log(clickMe.displayResults());
