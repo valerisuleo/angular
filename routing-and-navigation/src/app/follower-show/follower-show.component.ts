847d715266fda6a939a07014de75bfdd97597564
valeriorisuleo <valerio.risuleo@gmail.com>
2018-07-01 00:30:11 +0200

    section 10: routing and navigation

--- routing-and-navigation/src/app/follower-show/follower-show.component.ts
+++ routing-and-navigation/src/app/follower-show/follower-show.component.ts
@@ -11,36 +11,6 @@ import { FollowersService } from '../services/followers/followers.service';
 })
 export class FollowerShowComponent implements OnInit  {
 
-
-  // constructor(
-  //   private route: ActivatedRoute,
-  //   private service: FollowersService
-  // ) { }
-  //
-  // user = {};
-  //
-  // showFollower() {
-  //   const vm = this;
-  //   const id = vm.route.snapshot.paramMap.get('id');
-  //   console.log('id', id);
-  //
-  //
-  //   vm.service.get(id)
-  //   .subscribe((response) => {
-  //     vm.user = response;
-  //     console.log(response);
-  //   });
-  // }
-  //
-  // ngOnInit() {
-  //   const vm = this;
-  //
-  //   vm.showFollower();
-  //   console.log('vm', vm);
-  // }
-
-
-
   constructor(
     private route: ActivatedRoute,
     private service: FollowersService
@@ -48,8 +18,8 @@ export class FollowerShowComponent implements OnInit  {
 
   asso = {};
   data: any[];
-
   followerParams = +this.route.snapshot.paramMap.get('id');
+
 
   showFollower() {
     const vm = this;
@@ -63,13 +33,11 @@ export class FollowerShowComponent implements OnInit  {
     });
   }
 
-
   ngOnInit() {
     const vm = this;
 
     vm.showFollower()
     .then((response) => {
-      // console.log(response);
       vm.asso = response;
     });
     console.log('vm', vm);
