c60abf2eb0da951e90220955c1f64e88499149ae
valeriorisuleo <valerio.risuleo@gmail.com>
2018-08-21 00:10:14 +0200

    ricerca docs: building template-driven-form

--- mean/src/app/rdocumenti/index-docs/index-docs.component.ts
+++ mean/src/app/rdocumenti/index-docs/index-docs.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IndexDocsService } from '../../services/rdocumenti/index-docs.service';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'index-docs',
@@ -11,15 +14,41 @@ export class IndexDocsComponent implements OnInit {
 
   allDocs: any[];
 
+  model: any;
+
+  searchOptions = [
+    {id:1, optionName: 'Fax in'},
+    {id:2, optionName: 'Casella Postale'},
+    {id:3, optionName: 'Lavorato'},
+    {id:7, optionName: 'Cerca nel cestino'}
+  ];
+
+  canale = [
+    {id:4, optionName: 'business'},
+    {id:5, optionName: 'consumer'},
+    {id:6, optionName: 'high prioryty'}
+  ];
+
   constructor(private service: IndexDocsService) { }
 
-  ngOnInit() {
-    const vm = this;
 
-    vm.service.getAll()
-    .subscribe((response) => {
-      vm.allDocs = response.json();
-      console.log('allDocs', vm.allDocs);
-    });
+  log(asso) {
+    console.log('asso', asso);
+  }
+
+  submit(f) {
+    console.log('f', f.value)
+  }
+
+
+
+  ngOnInit() {
+    // const vm = this;
+    //
+    // vm.service.getAll()
+    // .subscribe((response) => {
+    //   vm.allDocs = response.json();
+    //   console.log('allDocs', vm.allDocs);
+    // });
   }
 }
