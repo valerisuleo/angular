c8574bcce247a5fa5959bf7539a5cad21551b3da
Valerio Risuleo <valerio.risuleo@gmail.com>
2020-06-30 12:20:30 +0100

    update repo

--- section_19-project/project/src/app/services/db-utlities.ts
+++
@@ -1,8 +0,0 @@
-export function snapsCoverter(snaps) {
-    
-    return snaps.map((rawData) => {
-        const obj = rawData.payload.doc.data();
-        obj.id = rawData.payload.doc.id;
-        return obj;
-    });
-}
